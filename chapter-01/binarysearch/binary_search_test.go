ebeb33e99d5285e9749d614dd0b3f516c5886f3c
Daniel <danielsantiago111@hotmail.com>
2022-09-14 14:26:25 -0300

    add test case to non-present element

--- chapter-01/binarysearch/binary_search_test.go
+++ chapter-01/binarysearch/binary_search_test.go
@@ -11,4 +11,13 @@ func TestSearchIndex(t *testing.T) {
 			t.Errorf("got %d want %d", got, want)
 		}
 	})
+
+	t.Run("should get -1 when an element is not present in an array", func(t *testing.T) {
+		got := SearchIndex([]int{-1, 0, 1, 2, 3, 4, 5}, 100)
+		want := -1
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
